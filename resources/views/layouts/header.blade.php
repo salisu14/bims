616293f9f0cc33cb1f5d8422c3e16a71e076f7ad
Muhammad S. Ali <22814048+salisu14@users.noreply.github.com>
2022-11-30 20:59:24 +0000

    Add BIMS heading in the header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,5 @@
 <header class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600">
+    <span class="hidden text-blue-500 text-2xl lg:block">Bifli Inventory Management System</span>
     <div class="flex items-center">
         <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
             <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
